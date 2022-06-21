b220ca84912c45be023f7b19256a37f2ef8bc8cd
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-06-21 11:41:59 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -13,6 +13,6 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
-expect(size("0")).equals('L');
-expect(size(0)).equals('L');
+expect(size(*)).equals('L');
+expect(size(L)).equals('L');
 console.log('All is well (maybe!)');
