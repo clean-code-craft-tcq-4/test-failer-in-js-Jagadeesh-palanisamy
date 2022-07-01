b8f1de84ded4e18d44249ac4adba53aaf97bcc4e
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-01 09:31:20 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,9 +1,10 @@
 const {expect} = require('chai')
+ const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+ const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+let colorSet= new Map();
 
 function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
+   for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
         }
@@ -11,8 +12,25 @@ function print_color_map() {
     return majorColors.length * minorColors.length;
 }
 
+function printColorMapStub() {
+     for (let i = 0; i < majorColors.length; i++) {
+        for (let j = 0; j < minorColors.length; j++) {
+    colorSet.set((i * 5 + j)+1,{'majorColor':majorColors[i],'minorColor':minorColors[j]});
+          }
+        }
+}
+function checkColorMap(colorCode,expectMajorColor,expectedMinorColor){
+  print_color_map();
+  let indivdualColorSet= colorSet.get(colorCode)
+  if(indivdualColorSet.majorColor===expectMajorColor && indivdualColorSet.minorColor===expectedMinorColor ){
+    console.log("Success")
+  }else{
+    throw 'Failure!! ColorCode and Colors are mismatch';
+  }
+  }
 result = print_color_map();
 expect(result).equals(25);
-expect(result).equals('25'); // need to check the data type of the object 
 expect(result).equals(0);
+checkColorMap(1,'White','Blue');
+checkColorMap(9,'Red','Brown');
 console.log('All is well (maybe!)');
